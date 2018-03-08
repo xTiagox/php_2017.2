98e7f88230a0f4c3acfc646f37b4c658d162ee42
Tiago Silva <>
2018-03-08 20:39:44 -0300

    Criação do projeto aula02

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Autor: Tiago Silva
+Local: Senac Rio
+Data: 06/03/2018
+Finalidade: Página principal do projeto
+-->
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 2</title>
+    </head>
+    <body>
+        <?php
+            $var1 = 10;
+            $var2 = 20;
+            $var3 = $var1 + $var2;
+            
+            
+            echo $var3. "<br>";
+        ?>
+    </body>
+</html>
